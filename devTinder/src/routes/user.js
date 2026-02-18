3e8faf35795c89d775efd8d44a3d8846b31ae1b8
abhimvp <abhishekreddyboddu1@gmail.com>
2026-02-18 17:07:31 +0530

    S2 | Episode-14 |Building Feed API & Pagination - Part 1 - Feed API

--- devTinder/src/routes/user.js
+++ devTinder/src/routes/user.js
@@ -52,4 +52,45 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    // user should see all the user cards except
+    // his own card
+    // his connections
+    // ignored people
+    // already send the connection request people
+
+    const loggedInUser = req.user;
+
+    // Find all connection requests ( sent + received) of the logged in user
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    })
+      .select("toUserId fromUserId status")
+      .populate("fromUserId", "firstName")
+      .populate("toUserId", "firstName"); // use select to get only the required fields from the connection requests collection, and this will help us to easily filter out the users who are connected, ignored or already sent the connection request by the logged in user when we query the connection requests collection & use populate to get the user data of the sender and receiver of the connection request, and we are selecting only the firstName field of the user to be populated in these fields, and this will help us to easily get the user data of the sender and receiver of the connection request when we query the connection requests collection
+
+    const hideUsersFromFeed = new Set(); // set is a data structure that stores unique values, and we are using it to store the user ids of the users who are connected, ignored or already sent the connection request by the logged in user, so that we can easily filter out these users from the feed of the logged in user
+
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.toUserId._id.toString());
+      hideUsersFromFeed.add(req.fromUserId._id.toString());
+    });
+
+    // console.log(hideUsersFromFeed);  shows the user ids of the users who are connected, ignored or already sent the connection request by the logged in user
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ], // use $nin operator to filter out the users who are connected, ignored or already sent the connection request by the logged in user when we query the users collection, and we are converting the set of user ids to an array using Array.from() method, and this will help us to easily filter out these users from the feed of the logged in user when we query the users collection
+    }).select(USER_SAFE_DATA); // use select to get only the required fields from the users collection, and this will help us to easily display the user data of the users in the feed of the logged in user when we query the users collection
+    res.send(users);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
 module.exports = { userRouter };
